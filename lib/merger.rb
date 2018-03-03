e65ea25be33ebf8cedceef934c4957547b7395d8
Yury Bogdanov <>
2018-03-03 22:13:36 +0300

    Comments
    Comments for further actions

--- lib/merger.rb
+++ lib/merger.rb
@@ -5,6 +5,12 @@ require "json"
 
 module Merger
   class Merger
+    # Rename this function to 'merge'
+    # What to do here:
+    # 1. Post greeting
+    # 2. Read user input (json)
+    # 3. Post info (data acquired)
+    # 4. Pass data found for files searching
     def self.begin_merge
       Poster.post_greeting
       puts "Searching for Mergefile.json in #{Dir.pwd}"
--- lib/merger/filemerger.rb
+++ lib/merger/filemerger.rb
@@ -1,6 +1,10 @@
 require 'merger/poster'
 
 module Merger
+  # Rename this class into Searcher
+  # This class should only search for files
+  # Create another one, which will be named Filemerger
+  # This class should only merge files
   class Filemerger
     def self.find_files(masks)
       first_files = Dir.glob("**/#{masks.first}")
