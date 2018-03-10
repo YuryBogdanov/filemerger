484edd22f7108d97cc8ab68bf62929bdc4a2f7c6
Yury Bogdanov <>
2018-03-10 12:07:38 +0300

    0.4.7
    Readme edited + minor typo fix

--- lib/filemerger/poster.rb
+++ lib/filemerger/poster.rb
@@ -37,7 +37,7 @@ module Filemerger
         puts "✅  The merge has been successfuly completed.".colorize(:green)
       else
         puts "⚠️  The merge has been completed with #{errors_count} errors".colorize(:yellow)
-        puts "    Please see the log above.".colorize(:yellow)
+        puts "   Please see the log above.".colorize(:yellow)
       end
     end
 
